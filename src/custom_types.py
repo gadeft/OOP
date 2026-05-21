0f744e03fba1625442265fe57f051c80da75dd0e
DESKTOP-H366173\SuperDuperMegaPC <dimaisay1109@gmail.com>
2026-05-21 19:55:19 +0300

    Practical work №8 done

--- src/custom_types.py
+++
@@ -1,5 +0,0 @@
-from typing import NewType
-from datetime import datetime
-
-
-type Metadata = NewType('Metadata', dict[str, str | int | float | bool | list | datetime | Metadata])
--- src/path_parser.py
+++ src/path_parser.py
@@ -7,3 +7,12 @@ def absolute_path(path: str) -> list[str] | None:
         path = path[:-1]
     result = path.split('/')[1:] # removing empty string at the start
     return result
+
+
+if __name__ == '__main__':
+    path = '/folder/another/one/text/'
+    # path = '/\\folder\\another\\one\\text'
+    # path = '/'
+    abs_path = absolute_path(path)
+    print(abs_path)
+    print(abs_path[0] == '')
